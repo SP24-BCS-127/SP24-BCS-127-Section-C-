6d3357297439ce3e45daa1d3ff9cb54ee34b5da2
SP24-BCS-127 <haseebahmad03156078199@gmail.com>
2026-03-12 22:35:24 +0500

    Delete Lab Assignment directory

--- Lab Assignment/lib/services/file_service.dart
+++
@@ -1,2 +0,0 @@
-export 'file_service_stub.dart'
-    if (dart.library.io) 'file_service_io.dart';
--- Lab Assignment/lib/widgets/file_image_stub.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-Widget buildFileImage(String path, {BoxFit fit = BoxFit.cover}) {
-  return const SizedBox.shrink();
-}
-
-bool checkFileExists(String path) {
-  return false;
-}
